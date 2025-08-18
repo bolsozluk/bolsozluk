12f91c5c409837c2d1e9805f80b98c3871ff79b7
Bol Sözlük <info@bolsozluk.com>
2025-08-18 13:40:46 +0200

    Update msjsil.php

--- icerik/msjsil.php
+++ icerik/msjsil.php
@@ -1,7 +1,11 @@
-<?
+
+<?php
+$id = isset($_REQUEST["id"]) ? guvenlikKontrol($_REQUEST["id"], "ultra") : "";
+$kullaniciAdi = isset($_SESSION['kullaniciAdi_S']) ? $_SESSION['kullaniciAdi_S'] : "";
 
 $sorgu = "DELETE FROM privmsg WHERE kime = '$kullaniciAdi' and id = '$id' LIMIT 1";
 mysql_query($sorgu);
-if (mysql_query($sorgu))
-Header ("Location: sozluk.php?process=privmsg");
+
+Header("Location: sozluk.php?process=privmsg");
+exit;
 ?>
