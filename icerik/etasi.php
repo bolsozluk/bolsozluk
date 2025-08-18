8258422fb6295068c7f3c47cae55c7b5a88225b9
Bol Sözlük <info@bolsozluk.com>
2025-08-18 13:18:45 +0200

    Update etasi.php

--- icerik/etasi.php
+++ icerik/etasi.php
@@ -1,63 +1,64 @@
-<?
-$id = guvenlikKontrol($_REQUEST["id"],"ultra");
-$sira = guvenlikKontrol($_REQUEST["sira"],"ultra");
-$sebep = guvenlikKontrol($_REQUEST["sebep"],"hard");
-$sr = guvenlikKontrol($_REQUEST["sr"],"ultra");
+
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$id = guvenlikKontrol($_REQUEST["id"], "ultra");
+$sira = guvenlikKontrol($_REQUEST["sira"], "ultra");
+$sebep = guvenlikKontrol($_REQUEST["sebep"], "hard");
+$sr = guvenlikKontrol($_REQUEST["sr"], "ultra");
 $kullaniciAdi = $_SESSION['kullaniciAdi_S'];
 $kulYetki = $_SESSION['kulYetki_S'];
 
-	//if ($dbyazar != $kullaniciAdi and $kulYetki != "admin" and $kulYetki != "mod")  {
-		if ($kulYetki != "admin" and $kulYetki != "mod")  {
-		$ip = getenv('REMOTE_ADDR');
-		echo "Dikkat!<br>$ip ispitledin!";
-		die;
-	}
-	
+if ($kulYetki != "admin" && $kulYetki != "mod") {
+    $ip = getenv('REMOTE_ADDR');
+    echo "Dikkat!<br>$ip ispitledin!";
+    die;
+}
+
 if (!$kullaniciAdi) {
- header("Location:sozluk.php?process=master&login=yescanim");
-} else {
- //if ($_GET['id'] and $_GET['sira'] and !$_POST['tasi']) {
- ?>
- <form action="" method="POST">
- #<?=$id?> numaralı entry'yi #<input type="number" size="5" name="sira"> numaralı başığa taşı!<br>
- <input type="submit" name="tasi" value="tamamdir">
- </form>
- <?
- if ($_POST['tasi'] and $sira>1) {
+    header("Location:sozluk.php?process=master&login=yescanim");
+    exit;
+}
 
-$sorgu1 = "SELECT sira FROM mesajlar WHERE `id` = '$id'";
-	$sorgu2 = mysql_query($sorgu1);
-	$kayit2=mysql_fetch_array($sorgu2);
-	$tasiorji=$kayit2["sira"];
+if ($_POST['tasi'] && $sira > 1) {
+    // Güncellenmeden önce eski sira alınır
+    $sorgu1 = "SELECT sira FROM mesajlar WHERE id = '$id'";
+    $sorgu2 = mysql_query($sorgu1);
+    $kayit2 = mysql_fetch_array($sorgu2);
+    $tasiorji = $kayit2["sira"];
 
- 			$sorgu = "UPDATE mesajlar SET `tasiorji` = '$tasiorji' WHERE id='$id'";
-		mysql_query($sorgu);
+    // Orijinal sıra kaydedilir
+    $sorgu = "UPDATE mesajlar SET tasiorji = '$tasiorji' WHERE id='$id'";
+    mysql_query($sorgu);
 
- $degistir=mysql_query("update mesajlar set sira='$_POST[sira]' where id='$_GET[id]'");
+    // Sıra güncellenir
+    $degistir = mysql_query("UPDATE mesajlar SET sira='$sira' WHERE id='$id'");
 
-$tarih = date("YmdHi");
- 		$sorgu = "UPDATE mesajlar SET `tasiyan` = '$kullaniciAdi' WHERE id='$id'";
-		mysql_query($sorgu);
+    // Taşıyan ve tarih kaydedilir
+    $tarih = date("YmdHi");
+    mysql_query("UPDATE mesajlar SET tasiyan = '$kullaniciAdi' WHERE id='$id'");
+    mysql_query("UPDATE mesajlar SET tasitarih = '$tarih' WHERE id='$id'");
 
-		$sorgu = "UPDATE mesajlar SET `tasitarih` = '$tarih' WHERE id='$id'";
-		mysql_query($sorgu);
+    $no = mysql_affected_rows(); // Doğru fonksiyon!
 
-
- $no=@mysql_num_rows($degistir);
-  echo "entryniz #$_POST[sira] numaralı başlığa taşınmıştır";
-?>
-<script language="JavaScript" type="text/javascript">
+    echo "entryniz #$sira numaralı başlığa taşınmıştır";
+    ?>
+    <script language="JavaScript" type="text/javascript">
         setTimeout("window.history.go(-2)",500);
-</script>
-<?
-
- }
-  if ($no>=1 or $sira<1) 
-  {
-  echo "sözlük sıçtı sonra tekrar deneyiniz, belki de kusur sözlükte değildir.";
-  } 
-   //} 
+    </script>
+    <?php
+}
 ?>
 
+<form action="" method="POST">
+  <input type="hidden" name="id" value="<?=$id?>">
+  #<?=$id?> numaralı entry'yi #<input type="number" size="5" name="sira"> numaralı başlığa taşı!<br>
+  <input type="submit" name="tasi" value="tamamdir">
+</form>
+<?php
 
-
+if (isset($no) && ($no < 1 || $sira < 1)) {
+    echo "sözlük sıçtı sonra tekrar deneyiniz, belki de kusur sözlükte değildir.";
+}
+?>
