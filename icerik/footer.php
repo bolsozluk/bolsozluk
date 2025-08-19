e7dc1a581242ac20b94338e46e3148852afc79f7
Bol Sözlük <info@bolsozluk.com>
2025-08-19 11:41:19 +0200

    Update footer.php

--- icerik/footer.php
+++ icerik/footer.php
@@ -3,25 +3,45 @@ $isMobile = (bool)preg_match('#\b(ip(hone|od|ad)|android|opera m(ob|in)i|windows
                     '|s(ymbian|eries60|amsung)|p(laybook|alm|rofile/midp|laystation portable)|nokia|fennec|htc[\-_]'.
                     '|mobile|up\.browser|[1-4][0-9]{2}x[1-4][0-9]{2})\b#i', $_SERVER['HTTP_USER_AGENT'] );
 ?>
-
-<center>
-<a href="https://chat.bolsozluk.com" target="_blank"><font size=1>chat</font></a>
- | 
-<a href="https://www.youtube.com/BolSozluk" target="_blank"><font size=1>youtube</font></a>
- | 
-<a href="https://open.spotify.com/artist/6cbqsKLbEyJZ7LhiuIqe7z" target="_blank"><font size=1>spotify</font></a>
- | 
-<a href="https://anket.bolsozluk.com" target="_blank"><font size=1>anket</font></a>
- | 
-<a href="https://www.bolsozluk.com/raple" target="_blank"><font size=1>raple</font></a>
- | 
-<a href="https://github.com/bolsozluk/" target="_blank"><font size=1>source</font></a>
- | 
-<a href="/sozlesme.html" target="_blank"><font size=1>uyarı</font></a>
- | 
-<a href="/devlog.txt" target="_blank"><font size=1>devlog</font></a></center>
-<br>
-<center>
-<font size=1>
-bol'da yer alan içeriğin doğru veya güncel olduğu hiçbir şekilde iddia veya garanti edilmemektedir. burada okuduklarınız sizi dehşete düşürürse türkçe rap ansiklopedisine de göz atmayı deneyebilirsiniz. <? if ($isMobile == 0) echo "<br>"; ?>  hukuka aykırı olabileceğini düşündüğünüz içerikler titizlikle incelenip gereği düşünülmektedir. sözlüğü reklamsız görüntülemek isterseniz üye girişi yapabilirsiniz. soğuk içiniz. (2014-2025)
-</center>
+<style>
+.footer {
+    text-align: center;
+    font-size: <?php echo $isMobile ? '12px' : '15px'; ?>;
+    margin-top: 20px;
+    color: #555;
+}
+.footer-links a {
+    color: #006699;
+    text-decoration: none;
+    margin: 0 6px;
+}
+.footer-links a:hover {
+    text-decoration: underline;
+}
+.footer-info {
+    font-size: 12px;
+    margin-top: 8px;
+    color: #888;
+}
+@media (max-width: 600px) {
+    .footer { font-size: 11px; }
+    .footer-info { font-size: 10px; }
+}
+</style>
+<div class="footer">
+    <div class="footer-links">
+        <a href="https://chat.bolsozluk.com" target="_blank">chat</a> |
+        <a href="https://www.youtube.com/BolSozluk" target="_blank">youtube</a> |
+        <a href="https://open.spotify.com/artist/6cbqsKLbEyJZ7LhiuIqe7z" target="_blank">spotify</a> |
+        <a href="https://anket.bolsozluk.com" target="_blank">anket</a> |
+        <a href="https://www.bolsozluk.com/raple" target="_blank">raple</a> |
+        <a href="https://github.com/bolsozluk/" target="_blank">source</a> |
+        <a href="/sozlesme.html" target="_blank">uyarı</a> |
+        <a href="/devlog.txt" target="_blank">devlog</a>
+    </div>
+    <div class="footer-info">
+        bol'da yer alan içeriğin doğru veya güncel olduğu hiçbir şekilde iddia veya garanti edilmemektedir. burada okuduklarınız sizi dehşete düşürürse türkçe rap ansiklopedisine de göz atmayı deneyebilirsiniz.
+        <?php if (!$isMobile) echo "<br>"; ?>
+        hukuka aykırı olabileceğini düşündüğünüz içerikler titizlikle incelenip gereği düşünülmektedir. sözlüğü reklamsız görüntülemek isterseniz üye girişi yapabilirsiniz. soğuk içiniz. (2014-2025)
+    </div>
+</div>
