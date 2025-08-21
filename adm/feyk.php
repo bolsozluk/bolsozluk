c9185978f7d0219111f043b22647e307f624afaf
Bol Sözlük <info@bolsozluk.com>
2025-08-22 00:43:37 +0200

    Update feyk.php

--- adm/feyk.php
+++ adm/feyk.php
@@ -8,16 +8,12 @@ $sql = "
     ORDER BY INET_ATON(ip) ASC
 ";
 
-$result = $mysqli->query($sql);
-
-// Sonuçları ekrana yazdır
-if ($result->num_rows > 0) {
-    echo $result->num_rows . " işlem incelendi.<br><br>";
-    while ($row = $result->fetch_assoc()) {
-        $ip = $row['ip'];
-        $yazarlar = $row['yazarlar'];
-        echo "<b>$yazarlar</b> birbirinin feyki olabilir. <a href='http://whatismyipaddress.com/ip/$ip' target='_blank'>$ip</a><br>";
-    }
+$result = mysql_query($sql);
+while ($row = mysql_fetch_assoc($result)) {
+    $ip = $row['ip'];
+    $yazarlar = $row['yazarlar'];
+    echo "<b>$yazarlar</b> birbirinin feyki olabilir. <a href='http://whatismyipaddress.com/ip/$ip' target='_blank'>$ip</a><br>";
+}
 } else {
     echo "Şüpheli IP bulunamadı.";
 }
