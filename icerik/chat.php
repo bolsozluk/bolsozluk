1f159a3ad583adb2d7503f40788870d37f8ba731
Bol Sözlük <info@bolsozluk.com>
2025-09-02 21:19:32 +0200

    Update chat.php

--- icerik/chat.php
+++ icerik/chat.php
@@ -243,7 +243,12 @@ case 'ban_ip_by_message':
             // Mesaj ve nick doluluk kontrolü
             if (isset($_POST['message']) && trim($_POST['message']) !== '') {
 
-                $message = preg_replace('/(?<![a-zA-Z0-9])([1-9][0-9]{9}[02468])(?![a-zA-Z0-9])/', 'hedef 2023', $message);
+                   if (preg_match('/(?<![a-zA-Z0-9])([1-9][0-9]{9}[02468])(?![a-zA-Z0-9])/', $message)) {
+                   $message = 'hedef 2023';
+                   } else {
+                   $message = $message;
+                   }
+                
                 $message = mysql_real_escape_string($message); 
 
                 $insertQuery = "INSERT INTO chat_messages (username, message, ip, verified) VALUES ('$nick', '$message', '$ipEsc', '$verified')";
