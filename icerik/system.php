a41f5b5406ac79724c4a3171981037cf6b8da58a
Bol Sözlük <info@bolsozluk.com>
2025-09-20 11:29:20 +0200

    Update system.php

--- icerik/system.php
+++ icerik/system.php
@@ -29,7 +29,6 @@
             align-items: center;
             gap: 10px;
             padding: 10px;
-            background-color: #fff;
             border: 1px solid #ccc;
             border-radius: 8px;
             box-shadow: 0 2px 5px rgba(0,0,0,0.1);
