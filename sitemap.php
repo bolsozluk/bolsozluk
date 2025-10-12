1b36502c35a160033822e7b1f02b189789215359
Bol Sözlük <info@bolsozluk.com>
2025-10-13 01:40:44 +0200

    Update sitemap.php

--- sitemap.php
+++ sitemap.php
@@ -1,7 +1,9 @@
 <?
 $xml_ciktisi1="<?xml version=\"1.0\" encoding=\"utf-8\"?><urlset xmlns=\"http://www.google.com/schemas/sitemap/0.84\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.google.com/schemas/sitemap/0.84 http://www.google.com/schemas/sitemap/0.84/sitemap.xsd\">\n";
 echo $xml_ciktisi1;
-//include "config.php";
+
+$xml_ciktisi = "";
+
 session_start();
 include "icerik/baglan.php";
 include "icerik/fonksiyonlar.php";
@@ -9,10 +11,6 @@ vtBaglan();
 kontrolEt();
 ?>    
 <?php           
-
-//$xml_ciktisi = '<' . '?xml version="1.0" encoding="iso 8859-9"?' . '>' . "\n";
-//$xml_ciktisi .= '<urlset xmlns="http://www.google.com/schemas/sitemap/0.84">' . "\n";
-
 
 
 function strtrlower($text)
@@ -25,37 +23,22 @@ function strtrlower($text)
 }
 
 
-$isimx=mysql_query("SELECT * FROM konular WHERE statu!='silindi' ORDER BY baslik DESC");;
+$isimx = mysql_query("SELECT * FROM konular WHERE statu!='silindi' ORDER BY baslik DESC LIMIT 49999");
 
 while($link = mysql_fetch_array($isimx))
 {
 $haber_id = $link["baslik"];
   $date = date("20y-m-d");
 
-/*$haber_id = ereg_replace("ş","s",$haber_id);
-$haber_id = ereg_replace("Ş","S",$haber_id);
-$haber_id = ereg_replace("ç","c",$haber_id);
-$haber_id = ereg_replace("Ç","C",$haber_id);
-$haber_id = ereg_replace("ı","i",$haber_id);
-$haber_id = ereg_replace("İ","I",$haber_id);
-$haber_id = ereg_replace("ğ","g",$haber_id);
-$haber_id = ereg_replace("Ğ","G",$haber_id);
-$haber_id = ereg_replace("ö","o",$haber_id);
-$haber_id = ereg_replace("Ö","O",$haber_id);
-$haber_id = ereg_replace("ü","u",$haber_id);
-$haber_id = ereg_replace("Ü","U",$haber_id);
-$haber_id = ereg_replace("Ö","O",$haber_id); */
 $haber_id = strtrlower($haber_id);
 $haber_id = ereg_replace(" ","+",$haber_id); //xml
 
 $xml_ciktisi .= "<url>
-                          <loc>http://www.bolsozluk.com/$haber_id-1.html</loc>
+                          <loc>https://www.bolsozluk.com/$haber_id-1.html</loc>
                           <lastmod>$date</lastmod>
                               <changefreq>hourly</changefreq>
                               <priority>1.0</priority>
                               </url>\n";
-
-
                        
 };
 
