1a526e334e2ae3e38b291e80d62c3b06cfd514c6
Bol Sözlük <info@bolsozluk.com>
2025-11-27 13:17:47 +0100

    Update login.php

--- icerik/login.php
+++ icerik/login.php
@@ -102,7 +102,7 @@ $kim = $kullaniciAdi;
 //entry id çek
 $kimse1=mysql_fetch_array(mysql_query("SELECT * from user where nick='$kim'"));
 $kimse = $kimse1["nick"];
-$saycaylak = $kimse1["saycaylak";
+$saycaylak = $kimse1["saycaylak]";
 
 $sor = mysql_query("select yazar,statu from mesajlar WHERE `yazar`='$kim' and `statu` = '' ");
 $kactop = mysql_num_rows($sor);
