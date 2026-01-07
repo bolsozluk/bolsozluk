55c76d670e94aaf9efe0364da9375ef46f175122
Bol Sözlük <info@bolsozluk.com>
2026-01-07 01:21:25 +0100

    Update left.php

--- left.php
+++ left.php
@@ -1841,7 +1841,8 @@ $sor = mysql_query("select id from mesajlar WHERE `sira`=$gid and `statu` = '' "
 
 case "kanal":
 echo "<br>";
-echo "<center><input type='button' onclick=\"location.href='left.php?list=today';\" value='gündem' class='butx'> <input type='button' onclick=\"location.href='left.php?list=konudisi';\" value='konudışı' class='butx'> <input type='button' onclick=\"location.href='left.php?list=lobi';\" value='#lobi' class='butx'> $ekmobile </center>  ";
+
+if ($isMobile == 0) echo "<center><input type='button' onclick=\"location.href='left.php?list=today';\" value='gündem' class='butx'> <input type='button' onclick=\"location.href='left.php?list=konudisi';\" value='konudışı' class='butx'> <input type='button' onclick=\"location.href='left.php?list=lobi';\" value='#lobi' class='butx'> $ekmobile </center>  ";
 
 //-----------------------------------------
 	// KANAL FİLTRE (GÜNDEM)
